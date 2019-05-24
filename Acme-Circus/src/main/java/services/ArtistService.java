d42d3c6a8e205a0ec86ae22abbcff6e3d0378965
David Morales Llucia <dml94@live.com>
2019-05-24 12:02:41 +0200

    RF 12.2

--- Acme-Circus/src/main/java/services/ArtistService.java
+++ Acme-Circus/src/main/java/services/ArtistService.java
@@ -1,71 +1,132 @@
-package services; 
 
-import java.util.Collection; 
+package services;
 
-import org.springframework.beans.factory.annotation.Autowired; 
-import org.springframework.stereotype.Service; 
-import org.springframework.transaction.annotation.Transactional; 
-import org.springframework.util.Assert; 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import miscellaneous.Utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import repositories.ArtistRepository;
+import security.Authority;
+import security.UserAccount;
+import domain.Artist;
+import domain.CreditCard;
+import forms.ArtistRegisterForm;
 
-import domain.Artist; 
-import domain.Attendee;
-
-@Service 
-@Transactional 
-public class ArtistService { 
+@Service
+@Transactional
+public class ArtistService {
 
 	//Managed repository -------------------
 	@Autowired
-	private ArtistRepository artistRepository;
-
+	private ArtistRepository	artistRepository;
 
 	//Supporting Services ------------------
 
+	@Autowired
+	private ActorService		actorService;
+
 
 	//COnstructors -------------------------
-	public ArtistService(){
+	public ArtistService() {
 		super();
 	}
 
-
 	//Simple CRUD methods--------------------
 
-	public Artist create(){
+	public Artist create() {
 		Artist result;
 
 		result = new Artist();
 
+		final UserAccount ua = new UserAccount();
+
+		result.setUserAccount(ua);
+
+		final Authority a = new Authority();
+		a.setAuthority(Authority.ARTIST);
+		final Collection<Authority> authorities = new ArrayList<Authority>();
+		authorities.add(a);
+		result.getUserAccount().setAuthorities(authorities);
+
+		final CreditCard creditCard = new CreditCard();
+		result.setCreditCard(creditCard);
+
 		return result;
 	}
 
-	public Collection<Artist> findAll(){
+	public Collection<Artist> findAll() {
 		Collection<Artist> result;
 
-		result = artistRepository.findAll();
+		result = this.artistRepository.findAll();
 
 		return result;
 	}
 
-	public Artist findOne(int artistId){
+	public Artist findOne(final int artistId) {
 		Artist result;
 
-		result = artistRepository.findOne(artistId);
+		result = this.artistRepository.findOne(artistId);
 
 		return result;
 	}
 
-	public void save(Artist artist){
+	public Artist save(final Artist artist) {
 		Assert.notNull(artist);
 
-		artistRepository.save(artist);
+		return this.artistRepository.save(artist);
 	}
 
-	public void delete(Artist artist){
-		artistRepository.delete(artist);
+	public void delete(final Artist artist) {
+		this.artistRepository.delete(artist);
 	}
-
 
 	//Other Methods--------------------
-} 
+
+	public Artist constructByForm(final ArtistRegisterForm attendeeRegisterForm) {
+		Assert.isTrue(attendeeRegisterForm.getPassword().equals(attendeeRegisterForm.getConfirmPassword()));
+		final Artist result = this.create();
+		final Collection<String> emails = this.actorService.findAllEmails();
+		final String email = attendeeRegisterForm.getEmail();
+		final boolean bEmail = !emails.contains(email);
+		Assert.isTrue(bEmail);
+
+		final Collection<String> accounts = this.actorService.findAllAccounts();
+		final UserAccount userAccount = result.getUserAccount();
+		final Boolean bAccount = !accounts.contains(attendeeRegisterForm.getUsername());
+		Assert.isTrue(bAccount);
+
+		final Md5PasswordEncoder pe = new Md5PasswordEncoder();
+		final String password = pe.encodePassword(attendeeRegisterForm.getPassword(), null);
+		userAccount.setPassword(password);
+		userAccount.setUsername(attendeeRegisterForm.getUsername());
+		result.setUserAccount(userAccount);
+
+		result.setAddress(attendeeRegisterForm.getAddress());
+		final CreditCard creditCard = result.getCreditCard();
+		creditCard.setBrandName(attendeeRegisterForm.getBrandName());
+		creditCard.setCvv(attendeeRegisterForm.getCvv());
+		creditCard.setExpirationMonth(attendeeRegisterForm.getExpirationMonth());
+		creditCard.setExpirationYear(attendeeRegisterForm.getExpirationYear());
+		creditCard.setHolderName(attendeeRegisterForm.getHolderName());
+		creditCard.setNumber(attendeeRegisterForm.getNumber());
+		final Boolean b = Utils.creditCardIsExpired(creditCard);
+		Assert.isTrue(!b);
+		result.setCreditCard(creditCard);
+
+		result.setEmail(attendeeRegisterForm.getEmail());
+		result.setName(attendeeRegisterForm.getName());
+		result.setPhone("+34 " + attendeeRegisterForm.getPhone());
+		result.setPhoto(attendeeRegisterForm.getPhoto());
+		result.setSurnames(attendeeRegisterForm.getSurnames());
+
+		result.setDni(attendeeRegisterForm.getDni());
+		return result;
+	}
+}
